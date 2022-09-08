89eb7365fead70ba0b6f0e8072479dd195e84a85
Rafael <vecchirafael@gmail.com>
2022-09-08 19:12:08 -0300

    idMarca e idModelo adicionados em veiculos

--- veiculos/veiculos_cadastro.dart
+++ veiculos/veiculos_cadastro.dart
@@ -8,12 +8,17 @@ class VeiculosCadastro{
     var veiculosCadastroInformacoesInstancia = VeiculosInformacoes();
     print('Id do veículo: ${idVeiculo}');
     veiculosCadastroInformacoesInstancia.codigoVeiculo = idVeiculo;
+    print('Informe o Id da marca');
+    veiculosCadastroInformacoesInstancia.idMarcaVeiculo = int.parse(stdin.readLineSync()!);
+    print('Informe o Id do modelo');
+    veiculosCadastroInformacoesInstancia.idModeloVeiculo = int.parse(stdin.readLineSync()!);
+    //adicionar condição criando classe de verificação
     print('Informe a quilometragem do veículo');
     veiculosCadastroInformacoesInstancia.kmRodadoVeiculo = double.parse(stdin.readLineSync()!);
     print('Informe o valor da diária');
     veiculosCadastroInformacoesInstancia.valorVeiculo = double.parse(stdin.readLineSync()!);
     listaVeiculosInformacoes.add(veiculosCadastroInformacoesInstancia);
     idVeiculo += 1;
-    
+
   }
 }
--- veiculos/veiculos_informacoes.dart
+++ veiculos/veiculos_informacoes.dart
@@ -2,5 +2,7 @@ class VeiculosInformacoes{
   int? codigoVeiculo;
   double? kmRodadoVeiculo;
   double? valorVeiculo;
+  int? idMarcaVeiculo;
+  int? idModeloVeiculo;
 
 }
