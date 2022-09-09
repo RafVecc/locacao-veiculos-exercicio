5de1fe46dd1111933441ccb63ad4c0f98d782218
Rafael <vecchirafael@gmail.com>
2022-09-09 20:08:35 -0300

    mudança na classe LocacaoInformacoes

--- locacao/locacao_informacoes.dart
+++ locacao/locacao_informacoes.dart
@@ -3,5 +3,8 @@ class LocacaoInformacoes{
   var dataSaidaLocacao;
   var dataEntregaLocacao;
   int? diasLocadosLocacao;
+  int? idVeiculoLocacao;
+  int? idClienteLocacao;
+  int? idFuncionarioLocacao;
 
 }
