51d39974f4d11f91d7a727c79f10cc9acc4b25f3
Rafael <vecchirafael@gmail.com>
2022-09-09 20:44:07 -0300

    relatorio_cliente linkado ao id do cliente

--- relatorios/relatorio_cliente.dart
+++ relatorios/relatorio_cliente.dart
@@ -1,16 +1,19 @@
+import 'dart:io';
+
 import '../clientes/cliente_cadastro.dart';
 import '../marca/marca_cadastro.dart';
 import '../modelo/modelo_cadastro.dart';
 
 
-
 class RelatoriosClientes{
   void relatoriosClientes(){
-    for(int i = 0; i < listaClienteInformacoes.length; i ++) {
-      print('Nome do cliente: ${listaClienteInformacoes[i].nomeCliente}'
-          'Marca: ${listaMarcaInformacoes[i].nomeMarca}'
-          'Modelo: ${listaModeloInformacoes[i].nomeModelo}'
-          'Total a ser pago: ${listaClienteInformacoes[i].totalPagarCliente}');
-    }
+    int? i;
+    print('Informe o Id do Cliente');
+    i = int.parse(stdin.readLineSync()!);
+    print('Nome do cliente: ${listaClienteInformacoes[i-1].nomeCliente}'
+          'Marca: ${listaMarcaInformacoes[i-1].nomeMarca}'
+          'Modelo: ${listaModeloInformacoes[i-1].nomeModelo}'
+          'Total a ser pago: ${listaClienteInformacoes[i-1].totalPagarCliente}');
+
   }
 }
