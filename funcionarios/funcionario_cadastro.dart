f8b67feb908476dad379cd1800b7753c7dfc6219
Rafael <vecchirafael@gmail.com>
2022-09-09 20:53:46 -0300

    id do funcionario adicionado

--- funcionarios/funcionario_cadastro.dart
+++ funcionarios/funcionario_cadastro.dart
@@ -3,13 +3,16 @@ import 'funcionario_informacoes.dart';
 
 var listaFuncionarioInformacoes = <FuncionariosInformacoes>[];
 class FuncionarioCadastro{
+  int idFuncionario = 1;
   void funcionarioCadastro(){
    var funcionarioCadastroInformacoesInstancia = FuncionariosInformacoes();
+   print('Id do cliente: ${idFuncionario}');
+   funcionarioCadastroInformacoesInstancia.codigoFuncionario = idFuncionario;
    print('Informe o nome do funcionário');
    funcionarioCadastroInformacoesInstancia.nomeFuncionario = stdin.readLineSync();
    print('Informe o telefone do funcinário');
    funcionarioCadastroInformacoesInstancia.telefoneFuncionario = double.parse(stdin.readLineSync()!);
    listaFuncionarioInformacoes.add(funcionarioCadastroInformacoesInstancia);
-
+    idFuncionario += 1;
   }
 }
