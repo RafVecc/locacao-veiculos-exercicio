09b272a935895665b2338b748881666855023ad7
Rafael <vecchirafael@gmail.com>
2022-09-09 20:02:42 -0300

    laço de repetição adicionado

--- locacao/locacao_total_pago.dart
+++ locacao/locacao_total_pago.dart
@@ -7,10 +7,12 @@ import '../clientes/cliente_informacoes.dart';
 class LocacaoTotalPago {
   void locacaoTotalPago() {
     var locacaoTotalPagar = ClienteInformacoes();
-    var diariaVeiculo = listaVeiculosInformacoes[i].valorVeiculo;
-    var diasLocacao = listaLocacaoInformacoes[i].diasLocadosLocacao;
-    var totalPagar = diasLocacao! * diariaVeiculo!;
-    locacaoTotalPagar.totalPagarCliente = totalPagar;
-    listaClienteInformacoes.add(locacaoTotalPagar);
+    for(int i = 0; i < listaClienteInformacoes.length; i++) {
+      var diariaVeiculo = listaVeiculosInformacoes[i].valorVeiculo;
+      var diasLocacao = listaLocacaoInformacoes[i].diasLocadosLocacao;
+      var totalPagar = diasLocacao! * diariaVeiculo!;
+      locacaoTotalPagar.totalPagarCliente = totalPagar;
+      listaClienteInformacoes.add(locacaoTotalPagar);
+    }
   }
 }
