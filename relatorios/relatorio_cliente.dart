e1b04c6a1c4284b7897cca185be301f726d7de89
Rafael <vecchirafael@gmail.com>
2022-09-09 20:04:28 -0300

    laços de repetições adicionados nos relatorios

--- relatorios/relatorio_cliente.dart
+++ relatorios/relatorio_cliente.dart
@@ -6,10 +6,11 @@ import '../modelo/modelo_cadastro.dart';
 
 class RelatoriosClientes{
   void relatoriosClientes(){
-    print('Nome do cliente: ${listaClienteInformacoes[i].nomeCliente}'
+    for(int i = 0; i < listaClienteInformacoes.length; i ++) {
+      print('Nome do cliente: ${listaClienteInformacoes[i].nomeCliente}'
           'Marca: ${listaMarcaInformacoes[i].nomeMarca}'
           'Modelo: ${listaModeloInformacoes[i].nomeModelo}'
           'Total a ser pago: ${listaClienteInformacoes[i].totalPagarCliente}');
-
+    }
   }
 }
--- relatorios/relatorio_funcionario.dart
+++ relatorios/relatorio_funcionario.dart
@@ -2,7 +2,9 @@ import '../funcionarios/funcionario_cadastro.dart';
 
 class RelatorioFuncionario{
   void relatorioFuncionario(){
-    print('Nome do funcionário: ${listaFuncionarioInformacoes[i].nomeFuncionario}'
-          'Total de locações realizadas: ${}');
+    for(int i = 0; i < listaFuncionarioInformacoes.length; i ++) {
+      print('Nome do funcionário: ${listaFuncionarioInformacoes[i].nomeFuncionario}'
+          'Total de locações realizadas: ');
+    }
   }
 }
