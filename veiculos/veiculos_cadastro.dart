a6b5e31bf3511da95c1d04ec5a26a0ad44ddfed8
Rafael <vecchirafael@gmail.com>
2022-09-09 20:03:33 -0300

    verificação adicionada em veiculos_cadastro

--- veiculos/veiculos_cadastro.dart
+++ veiculos/veiculos_cadastro.dart
@@ -1,24 +1,32 @@
 import 'dart:io';
+import '../verificacoes/verificacoes_veiculos.dart';
 import 'veiculos_informacoes.dart';
 
+
 var listaVeiculosInformacoes = <VeiculosInformacoes>[];
+
 class VeiculosCadastro{
   int idVeiculo = 1;
   void veiculosCadastro(){
     var veiculosCadastroInformacoesInstancia = VeiculosInformacoes();
+    var verificacaoVeiculosInstancia = VerificacoesVeiculos();
     print('Id do veículo: ${idVeiculo}');
     veiculosCadastroInformacoesInstancia.codigoVeiculo = idVeiculo;
     print('Informe o Id da marca');
     veiculosCadastroInformacoesInstancia.idMarcaVeiculo = int.parse(stdin.readLineSync()!);
     print('Informe o Id do modelo');
     veiculosCadastroInformacoesInstancia.idModeloVeiculo = int.parse(stdin.readLineSync()!);
-    //adicionar condição criando classe de verificação
-    print('Informe a quilometragem do veículo');
-    veiculosCadastroInformacoesInstancia.kmRodadoVeiculo = double.parse(stdin.readLineSync()!);
-    print('Informe o valor da diária');
-    veiculosCadastroInformacoesInstancia.valorVeiculo = double.parse(stdin.readLineSync()!);
-    listaVeiculosInformacoes.add(veiculosCadastroInformacoesInstancia);
-    idVeiculo += 1;
-
+    if(verificacaoVeiculosInstancia.verificacaoIdMarcaVeiculo() == true &&
+        verificacaoVeiculosInstancia.verificacaoIdModeloVeiculo() == true
+    ){
+      print('Informe a quilometragem do veículo');
+      veiculosCadastroInformacoesInstancia.kmRodadoVeiculo =
+          double.parse(stdin.readLineSync()!);
+      print('Informe o valor da diária');
+      veiculosCadastroInformacoesInstancia.valorVeiculo =
+          double.parse(stdin.readLineSync()!);
+      listaVeiculosInformacoes.add(veiculosCadastroInformacoesInstancia);
+      idVeiculo += 1;
+    }
   }
 }
