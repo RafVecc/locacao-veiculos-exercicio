90731201dd1bc0e0111ae37eb01b3a8656176bdf
Rafael <vecchirafael@gmail.com>
2022-09-09 21:39:17 -0300

    contador de funcionários adicionado em relatorio_funcionario

--- relatorios/relatorio_funcionario.dart
+++ relatorios/relatorio_funcionario.dart
@@ -1,10 +1,26 @@
+import 'dart:io';
+
 import '../funcionarios/funcionario_cadastro.dart';
+import '../locacao/locacao_cadastro.dart';
 
 class RelatorioFuncionario{
   void relatorioFuncionario(){
+    int? idFuncionarioContador;
+    print('Informe o funcionário');
+    idFuncionarioContador = int.parse(stdin.readLineSync()!);
     for(int i = 0; i < listaFuncionarioInformacoes.length; i ++) {
-      print('Nome do funcionário: ${listaFuncionarioInformacoes[i].nomeFuncionario}'
-          'Total de locações realizadas: ');
+      int contadorFuncionario = 0;
+      for(int k = 0; k < listaLocacaoInformacoes.length; k ++) {
+        if (listaLocacaoInformacoes[k].idFuncionarioLocacao ==
+            listaFuncionarioInformacoes[i].codigoFuncionario) {
+          contadorFuncionario += 1;
+        }
+      }
+      int? x;
+      print('Informe o Id do Funcionário');
+      x = int.parse(stdin.readLineSync()!);
+      print('Nome do funcionário: ${listaFuncionarioInformacoes[x-1].nomeFuncionario}'
+          'Total de locações realizadas: ${contadorFuncionario}');
     }
   }
 }
